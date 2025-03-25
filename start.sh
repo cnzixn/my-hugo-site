9d40042d92ce540ae5a1b4232861fe3c8697ec29
cnzixn <cnzixn@gmail.com>
2025-03-25 23:38:56 +0800

    Update main at 2025-03-25 23:38:56

--- start.sh
+++ start.sh
@@ -8,6 +8,10 @@ rm -rf public; hugo --minify --noBuildLock
 hugo new posts/log-update-250301.md  --noBuildLock 
 
 
+
+# 在线编辑后，需要拉取到本地
+git pull
+
 # 提交更新
 git add .
 git commit -m "Update main at $(date '+%Y-%m-%d %H:%M:%S')"
